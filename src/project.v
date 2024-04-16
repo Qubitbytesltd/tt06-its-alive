cc6590136a12225fa3a9ae6298cc94b79b523b76
BlueWaterCrystal <55627513+BlueWaterCrystal@users.noreply.github.com>
2024-04-16 14:22:23 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Qubitbytes Ltd
  * SPDX-License-Identifier: Apache-2.0
  */
 
-`define default_netname none
+`default_nettype none
 
-module tt_um_example (
+module tt_um_qubitbytes_alive (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,9 +16,18 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // configure reset button
+  wire reset = ! rst_n;
+
+  // configure led segment
+  wire [7] led_out;
+  assign uo_out[7:0] = led_out; 
+
+    
+// configure speaker
+    assign uio_oe = 8'b11111111;
+    assign uio_out[0] = speaker;
+    assign uio_out[1] = ~speaker;
+    assign uio_out[7:2] = 0;
 
 endmodule
