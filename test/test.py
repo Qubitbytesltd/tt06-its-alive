16f00da25605e8794cdba9c30f10b326ea9b3053
BlueWaterCrystal <55627513+BlueWaterCrystal@users.noreply.github.com>
2024-04-17 05:01:46 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -18,7 +18,7 @@ async def test_project(dut):
   dut.ena.value = 1
   dut.ui_in.value = 0
   dut.uio_in.value = 0
-  dut.rst_n.value = 0
+  dut.rst_n.value = 1
  # await ClockCycles(dut.clk, 10) # 10 second delay
  # dut.rst_n.value = 1
  # await ClockCycles(dut.clk, 10) # 10 second delay
